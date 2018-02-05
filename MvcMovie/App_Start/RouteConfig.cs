82f0c0a531df5fa72e035954328d41f825bbdad1
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-02-05 21:52:23 +1100

    Update links and routes to work across controllers

--- MvcMovie/App_Start/RouteConfig.cs
+++ MvcMovie/App_Start/RouteConfig.cs
@@ -9,17 +9,17 @@ namespace MvcMovie
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // Hello
+            routes.MapRoute(
+                name: "Hello",
+                url: "{controller}/{action}/{name}/{id}"
+            );
+
             // Default
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-
-            // Hello
-            routes.MapRoute(
-                name: "Hello",
-                url: "{controller}/{action}/{name}/{id}"
             );
         }
     }
