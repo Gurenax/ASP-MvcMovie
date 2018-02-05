3a8c14e04e9d245274d2c54057de55afb6789e25
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-02-05 19:47:35 +1100

    Create HelloWorldController class

--- MvcMovie/App_Start/RouteConfig.cs
+++ MvcMovie/App_Start/RouteConfig.cs
@@ -9,6 +9,15 @@ namespace MvcMovie
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // e.g. https://www.codeproject.com/Articles/641783/Customizing-Routes-in-ASP-NET-MVC
+            // HelloWorld
+            //routes.MapRoute(
+            //    name: "HelloWorld",
+            //    url: "HelloWorld/{action}/{id}",
+            //    defaults: new { controller = "HelloWorld", action = "All", id = UrlParameter.Optional }
+            //);
+
+            // Default
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "<div>This is my <strong>default</strong> action...</div>";
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        //public ActionResult Index()
+        //{
+        //    return View ();
+        //}
+    }
+}
